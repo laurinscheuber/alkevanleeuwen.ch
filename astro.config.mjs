de9f27a7df43e0f84024ad560a0eb47b1c1546ee
laurinscheuber <38795964+laurinscheuber@users.noreply.github.com>
2026-02-08 23:13:00 +0100

    feat(seo): implement sitemap generation
    - install @astrojs/sitemap
    
    - configure site url
    
    - exclude studio routes from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://physiotherapie-alkevanleeuwen.ch',
+  integrations: [tailwind(), sitemap({
+    filter: (page) => !page.includes('/studio'),
+  })],
   vite: {
     server: {
       proxy: {
